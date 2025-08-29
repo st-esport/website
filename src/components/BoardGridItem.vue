bdcea4a1e02db2975f35ebf54fd5af02e4063224
Mads Jon Nielsen <madsjon@gmail.com>
2025-08-29 12:05:21 +0200

    Start using components, at least for grid item

---
+++ src/components/BoardGridItem.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps(['title', 'name', 'photo', 'phone', 'email'])
+</script>
+
+<template>
+  <div class="uk-card uk-card-default uk-card-body">
+    <img class="uk-border-rounded uk-width-medium" :src="photo" :alt="name"/><br>
+    <div class="uk-text-left">
+      {{ title }}<br>
+      <b>{{ name }}</b><br>
+      <a :href="'tel:' + phone">{{ phone }}</a><br>
+      <a :href="'mailto:' + email">{{ email }}</a><br>
+    </div>
+  </div>
+</template>
--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import BoardGridItem from '@/components/BoardGridItem.vue';
 </script>
 
 <template>
-  <main>
-  </main>
+  <div class="uk-container">
+    <div class=" uk-grid-small uk-text-center uk-flex-center">
+      <BoardGridItem title="Formand" name="Ole Kallehave" photo="/assets/board/ole.jpg" phone="+45 30201071" email="ole@rocha.dk"/>
+      <BoardGridItem title="Næstformand" name="Mads Kramme" photo="/assets/board/mads_kramme.jpg" phone="+45 30200909" email="mads@rohdekramme.dk"/>
+    </div>
+  </div>
 </template>
+
