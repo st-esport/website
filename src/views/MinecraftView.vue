f86839c0195e856be94b48b92e4178c256579f37
Mads Jon Nielsen <madsjon@gmail.com>
2025-09-13 16:10:08 +0200

    Fix text

--- src/views/MinecraftView.vue
+++ src/views/MinecraftView.vue
@@ -10,7 +10,7 @@ defineProps({
   <h1>Minecraft</h1>
   <div>
     <p>Minecraft er et uendeligt digitalt lærred, hvor dit barns fantasi kan blomstre og blive til vigtige færdigheder i den virkelige verden.</p>
-    <p>Vores trænere vil forsøge at guide denne kreative energi og omdanne den til struktureret læring.</p>
+    <p>Vores trænere vil guide denne kreative energi og omdanne den til struktureret læring.</p>
     <p>Der spilles torsdag fra 17:30 til 19:30, kig endelig forbi.</p>
   </div>
 </template>
