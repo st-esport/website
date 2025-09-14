5bf068faeb2c8303a42ba0f8c707ec988f22b07f
Ole Kallehave <ole@rocha.dk>
2025-09-14 22:09:25 +0200

    Update CounterStrikeView.vue

--- src/views/CounterStrikeView.vue
+++ src/views/CounterStrikeView.vue
@@ -5,7 +5,7 @@ import SiteContainer from "@/components/SiteContainer.vue";
 <template>
   <SiteContainer title="Counter-Strike" photo="/csgo_banner.jpg">
     <p>Dit barns passion for Counter-Strike er mere end bare en hobby.</p>
-    <p>Vores CS:GO træning omdanner spilletid til en stærk læringsoplevelse, hvor den digitale arena bliver en træningsbane for vigtige færdigheder i den virkelige verden.</p>
+    <p>Vores CS2 træning omdanner spilletid til en stærk læringsoplevelse, hvor den digitale arena bliver en træningsbane for vigtige færdigheder i den virkelige verden.</p>
     <p>Der spilles onsdag fra 17:30 til 19:30, kig endelig forbi.</p>
   </SiteContainer>
 </template>
