aebb8a74f3c7478df3a9040998fa771d34c4c477
Mads Jon Nielsen <madsjon@gmail.com>
2025-09-14 12:33:15 +0000

    Fix fortnite text

--- src/views/FortniteView.vue
+++ src/views/FortniteView.vue
@@ -6,6 +6,6 @@ import SiteContainer from "@/components/SiteContainer.vue";
   <SiteContainer title="Fortnite" photo="/fortnite_banner.jpg">
     <p>Elsker dit barn at spille Fortnite?</p>
     <p>Vi forvandler den passion til en positiv og struktureret fritidsaktivitet og tilbyder et trygt miljø, hvor gaming bliver et redskab til at lære værdifulde færdigheder.</p>
-    <p>Der spilles tirsdag og onsdag fra 17:30 til 19:30, kig endelig forbi.</p>
+    <p>Der spilles mandag og tirsdag fra 17:30 til 19:30, kig endelig forbi.</p>
   </SiteContainer>
 </template>
