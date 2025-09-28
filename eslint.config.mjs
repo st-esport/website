bd5a5323473b1a1292fbdd9934eb1b18a74ba4a8
Mads Jon Nielsen <madsjon@gmail.com>
2025-09-28 13:31:48 +0200

    Add eslint gha (#10)
    Co-authored-by: Mads Jon Nielsen <mjn@cego.dk>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,4 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 import { globalIgnores } from "eslint/config";
 
-export default withNuxt([globalIgnores([".worker-configuration.d.ts"])])
+export default withNuxt([globalIgnores(["worker-configuration.d.ts"])])
