a3a020ded254623ba893d1c7b5657b2fcb07b766
Mads Jon Nielsen <madsjon@gmail.com>
2025-09-28 13:22:50 +0200

    Improve SEO. Add uikit and eslint nuxt modules. (#9)
    Co-authored-by: Mads Jon Nielsen <mjn@cego.dk>

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,4 @@
 <script setup lang="ts">
-import 'uikit/dist/css/uikit.min.css';
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
-
-UIkit.use(Icons)
 </script>
 
 <template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ["nitro-cloudflare-dev"],
+  modules: ["nitro-cloudflare-dev", "@fedorae/nuxt-uikit", '@nuxt/eslint'],
+
+  app: {
+    head: {
+      title: 'Støvring eSport',
+      meta: [
+        { name: 'description', content: 'Fremmer børns sociale kundskaber og it færdigheder igennem gaming.' },
+        { name: 'keywords', content: 'støvring, esport, gaming, fortnite, minecraft, csgo' },
+      ],
+      htmlAttrs: {
+        lang: 'da',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
 
   nitro: {
     preset: "cloudflare_module",
