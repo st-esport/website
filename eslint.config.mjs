4a4e0071dba7baadc088fd878cec44ad1563a174
Mads Jon Nielsen <mjn@cego.dk>
2025-09-28 13:08:50 +0200

    Revert worker-configuration.d.ts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import { globalIgnores } from "eslint/config";
 
-export default withNuxt()
+export default withNuxt([globalIgnores([".worker-configuration.d.ts"])])
