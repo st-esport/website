fbfd88657601d24afeb9849f01d1e3f8d17108c2
Ole Kallehave <ole@rocha.dk>
2026-02-10 22:45:18 +0100

    Add facebook page element

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import SiteContainer from "@/components/SiteContainer.vue";
+import {RouterLink} from "vue-router";
 </script>
 
 <template>
@@ -9,11 +10,12 @@ import SiteContainer from "@/components/SiteContainer.vue";
     <p>Tryk <RouterLink to="tilmeld">tilmeld</RouterLink>, hvis du ønsker at tilmelde dig. Det koster 500 kr. hvert halve år.</p>
     <p>Har du spørgsmål eller ønsker at hjælpe til i foreningen, så kontakt os endelig på <a href="https://www.facebook.com/stoevringesport">https://www.facebook.com/stoevringesport</a></p>
 
-      <div class="fb-page"
-           data-tabs="timeline"
-           data-href="https://www.facebook.com/stoevringesport"
-           data-width="500"
-           data-hide-cover="false"></div>
+      <iframe
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fstoevringesport&tabs=timeline&width=500&height=700&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true"
+          width="500" height= "700" style="border:none;overflow:hidden"
+          allowfullscreen
+          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+      </iframe>
 
   </SiteContainer>
 </template>
