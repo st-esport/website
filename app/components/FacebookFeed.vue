701b0e8c0c41aca9794230f5d5caa8f4f4b02b9b
Ole Kallehave <ole@rocha.dk>
2026-02-11 22:40:51 +0100

    Add calendar component

--- app/components/FacebookFeed.vue
+++ app/components/FacebookFeed.vue
@@ -1,36 +1,21 @@
 <script setup lang="ts">
-import { ref, computed, onMounted, onBeforeUnmount } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const feedContainer = ref<HTMLElement | null>(null)
-const containerWidth = ref(500)
-
-const fbSrc = computed(() => {
-  const w = Math.min(containerWidth.value, 500)
-  return `https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fstoevringesport&tabs=timeline&width=${w}&height=700&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true`
-})
-
-let resizeObserver: ResizeObserver | undefined
+const fbSrc = ref('')
 
 onMounted(() => {
   if (feedContainer.value) {
-    containerWidth.value = feedContainer.value.clientWidth
-    resizeObserver = new ResizeObserver((entries) => {
-      for (const entry of entries) {
-        containerWidth.value = entry.contentRect.width
-      }
-    })
-    resizeObserver.observe(feedContainer.value)
+    const w = Math.min(feedContainer.value.clientWidth, 500)
+    fbSrc.value = `https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fstoevringesport&tabs=timeline&width=${w}&height=700&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true`
   }
-})
-
-onBeforeUnmount(() => {
-  resizeObserver?.disconnect()
 })
 </script>
 
 <template>
   <div ref="feedContainer" class="facebook-feed">
     <iframe
+      v-if="fbSrc"
       :src="fbSrc"
       height="700"
       style="border: none; overflow: hidden"
